723f5ddbfdf0637d066e443298cd9318652cc1bc
Michalek007 <nmichal02@op.pl>
2024-02-21 23:55:32 +0100

    python->rewrite uart.py & vhdl_parser.py in functional style, added: uart_init, uart_send_digit_image, transpose_weights, parse_weights_to_vhdl, parse_normalization_params_to_vhdl functions

--- python/uart.py
+++ python/uart.py
@@ -3,30 +3,54 @@ import serial
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-from utils import one_hot_encoder, prepare_mnist_data
-
-
-x = plt.imread('digit_images_samples\\img_1.jpg')
-y = [2]
-
-x = np.array([x])
-x, y = prepare_mnist_data(x, y)
-
-uart = serial.Serial(port='COM13', baudrate=19200, bytesize=8, parity=serial.PARITY_NONE,
-                     stopbits=serial.STOPBITS_ONE, timeout=0)
-
-print('True digit: ')
-print(np.argmax(y[0]))
+from utils import prepare_mnist_data
 
 
-i = 0
-for value in x[0][0]:
-    i += 1
-    value = int(value)
-    uart.write(value.to_bytes(1, byteorder='big'))
-    if i % 3 == 0:
-        time.sleep(0.1)
-        output = int.from_bytes(uart.read_all(), byteorder='big', signed=False)
-        print('\nPredicted digit: ')
-        print(output)
+def uart_init():
+    """ UART initialisation. Creates serial.Serial object.
+
+        Returns:
+            serial.Serial object
+    """
+    uart = serial.Serial(port='COM13', baudrate=19200, bytesize=8, parity=serial.PARITY_NONE,
+                         stopbits=serial.STOPBITS_ONE, timeout=0)
+    return uart
+
+
+def uart_send_digit_image(file_name: str):
+    """ Sends encoded digit image via UART.
+
+        Args:
+            file_name: 28x28 pixels image with digit number as name
+
+        Returns:
+            tuple of predicted digit collected via UART and true digit
+    """
+    uart = uart_init()
+
+    x = plt.imread(file_name)
+
+    y = file_name.split('\\')[-1].split('.')[0]
+    y = [int(y)]
+
+    x = np.array([x])
+    x, y = prepare_mnist_data(x, y)
+
+    print('True digit: ')
+    print(np.argmax(y[0]))
+
+    for value in x[0][0]:
+        value = int(value)
+        print(value)
+        uart.write(value.to_bytes(1, byteorder='big'))
+
+    time.sleep(0.1)
+    output = int.from_bytes(uart.read_all(), byteorder='big', signed=False)
+    print('\nPredicted digit: ')
+    print(output)
+
+    return output, y
+
+
+if __name__ == '__main__':
+    uart_send_digit_image('digit_images_samples\\0.jpg')
--- python/vhdl_parser.py
+++ python/vhdl_parser.py
@@ -1,68 +1,106 @@
 import json
-
 import numpy as np
 
-with open('models\\bnn_mnist_model.json', 'r') as f:
-    json_model = json.loads(f.read())
 
-all_weights = []
-for obj in json_model:
-    if obj.get('type') != 'dense':
-        continue
+def transpose_weights(json_model):
+    """ Transposes layers weights extract from json BNN model.
 
-    w_vector = obj.get('weights')
-    weights = [list() for _ in range(len(w_vector[0]))]
-    for row in w_vector:
-        n = len(row)
-        for i in range(n):
-            weights[i].append(row[i])
-    all_weights.append(weights)
+        Args:
+            json_model: json BNN model
 
-
-weights_str = '\n'
-for w_vector in all_weights:
-    i = 0
-    weights_str += '\n'
-    for row in w_vector:
-        if type(row) != list:
+        Returns:
+            transposed weights list
+    """
+    all_weights = []
+    for obj in json_model:
+        if obj.get('type') != 'dense':
             continue
 
-        weights_str += f'W({i})<="'
-        for value in reversed(row):
-            if value == 1:
-                weights_str += '1'
-            else:
-                weights_str += '0'
-        i += 1
-        weights_str += '";\n'
-print(weights_str)
+        w_vector = obj.get('weights')
+        weights = [list() for _ in range(len(w_vector[0]))]
+        for row in w_vector:
+            n = len(row)
+            for i in range(n):
+                weights[i].append(row[i])
+        all_weights.append(weights)
+    return all_weights
 
-normalization_params_str = '\n'
-for obj in json_model:
-    if obj.get('type') != 'batch_normalization':
-        continue
 
-    normalization_params_str += '\n'
-    values_array = obj.get('weights')
-    for i in range(len(values_array)):
-        values = np.array(values_array[i])
-        if i == 0 or i == 1:
-            values = values * 1000
-        values = values.astype('int32')
+def parse_weights_to_vhdl(weights: list):
+    """ Parses given weights list to VHDL code, equivalent to assigning value to signal W.
 
-        if i == 0:
-            normalization_params_str += 'signal GAMMA : NORMALIZATION_ARRAY :='
-        if i == 1:
-            normalization_params_str += 'signal BETA : NORMALIZATION_ARRAY :='
-        if i == 2:
-            normalization_params_str += 'signal MEAN : NORMALIZATION_ARRAY :='
-        if i == 3:
-            values[values == 0] = 1
-            normalization_params_str += 'signal STD_DEV : NORMALIZATION_ARRAY :='
+        Args:
+            weights: list of layers weights
 
-        values_str = str(values.tolist()).lstrip('[').rstrip(']')
-        values_str = ' (' + values_str + ')'
-        normalization_params_str += values_str + ';\n'
+        Returns:
+            parsed VHDL code string
+    """
+    weights_str = '\n'
+    for w_vector in weights:
+        i = 0
+        weights_str += '\n'
+        for row in w_vector:
+            if type(row) != list:
+                continue
 
-print(normalization_params_str)
+            weights_str += f'W({i})<="'
+            for value in reversed(row):
+                if value == 1:
+                    weights_str += '1'
+                else:
+                    weights_str += '0'
+            i += 1
+            weights_str += '";\n'
+    print(weights_str)
+    return weights_str
 
+
+def parse_normalization_params_to_vhdl(json_model):
+    """ Parses normalization params extracted from json BNN model to VHDL code,
+        equivalent to initialising signals GAMMA, BETA, MEAN, STD_DEV with type NORMALIZATION_ARRAY.
+
+        Args:
+            json_model: json BNN model
+
+        Returns:
+            parsed VHDL code string
+    """
+    normalization_params_str = '\n'
+    for obj in json_model:
+        if obj.get('type') != 'batch_normalization':
+            continue
+
+        normalization_params_str += '\n'
+        values_array = obj.get('weights')
+        for i in range(len(values_array)):
+            values = np.array(values_array[i])
+            if i == 0 or i == 1:
+                values = values * 1000
+            values = values.astype('int32')
+
+            if i == 0:
+                normalization_params_str += 'signal GAMMA : NORMALIZATION_ARRAY :='
+            if i == 1:
+                normalization_params_str += 'signal BETA : NORMALIZATION_ARRAY :='
+            if i == 2:
+                normalization_params_str += 'signal MEAN : NORMALIZATION_ARRAY :='
+            if i == 3:
+                values[values == 0] = 1
+                normalization_params_str += 'signal STD_DEV : NORMALIZATION_ARRAY :='
+
+            values_str = str(values.tolist()).lstrip('[').rstrip(']')
+            values_str = ' (' + values_str + ')'
+            normalization_params_str += values_str + ';\n'
+
+        print(normalization_params_str)
+        return normalization_params_str
+
+
+if __name__ == '__main__':
+    with open('models\\bnn_mnist_model.json', 'r') as f:
+        json_model = json.loads(f.read())
+
+    layers_weights = transpose_weights(json_model=json_model)
+    parse_weights_to_vhdl(weights=layers_weights)
+
+    parse_normalization_params_to_vhdl(json_model=json_model)
